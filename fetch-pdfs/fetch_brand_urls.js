ab48597c868bc804e976cb8d3e3d38156e1d3dea
michalisc <michalis@ravn.co.uk>
2014-12-18 17:47:50 +0000

    Retrives the Brand and link to Brand

--- fetch-pdfs/fetch_brand_urls.js
+++ fetch-pdfs/fetch_brand_urls.js
@@ -6,10 +6,12 @@
 
 var page = new WebPage(), testindex = 0, loadInProgress = false;
 var args = require('system').args;
+var brandLinks = []
 
 //fetch_url = args[1];
 
 url = "http://www.momentive.com/products/SelectorLanding.aspx?tid=1792"
+
 
 console.log("Fetching:" + url);
 page.onConsoleMessage = function (msg) {
@@ -24,22 +26,43 @@ page.onLoadStarted = function () {
 page.onLoadFinished = function () {
     loadInProgress = false;
     console.log("load finished");
+
 };
 
 page.onUrlChanged = function (url) {
     console.log('Following URL: ' + url);
 }
 
+// Get the links to each brand
 page.open(url, function(status) {
-    // list all the a.href links in the hello kitty etsy page
-    var links = page.evaluate(function() {
-        return [].map.call(document.querySelectorAll('a.selectorLandingItemLink'), function(link) {
+    page.evaluate(function() {
+        brandLinks =  [].map.call(document.querySelectorAll('a.selectorLandingItemLink'), function(link) {
             return [ link.innerHTML, link.getAttribute('href')];
-            //return link.getAttribute('href');
+        });
+        console.log("Brand Links found:" + brandLinks.length);
+        console.log(brandLinks.join('\n'));
+
+
+        // Go through the brands and get the product links and msds links
+        brandLinks.forEach(function(entry) {
+            brand = entry[0];
+            link = "http://www.momentive.com" + entry[1]
+            console.log(brand)
+            console.log(link)
+
+
+            page.open(link, function(status) {
+                var gradeLinks = page.evaluate(function() {
+                    return [].map.call(document.querySelectorAll('a.searchResultLink'), function(link) {
+                        return  link.getAttribute('href');
+                    });
+                });
+                console.log("Grade Links found:" + gradeLinks.length);
+                console.log(gradeLinks.join('\n'));
+            });
         });
     });
-    console.log(links.join('\n'));
-
-    phantom.exit();
 });
+
+
 
