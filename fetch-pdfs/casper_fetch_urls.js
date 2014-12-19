bbfd218dc868e64f63f1f914d1225c1cb4a74731
michalisc <michalis@ravn.co.uk>
2014-12-19 11:29:06 +0000

    Retrives the Brand and links to products and outputs to file

--- fetch-pdfs/casper_fetch_urls.js
+++ fetch-pdfs/casper_fetch_urls.js
@@ -4,12 +4,14 @@
  * Date: 18/12/2014
  */
 var casper = require('casper').create();
+var fs = require('fs');
+var fname = 'urls.txt';
+var save = fs.pathJoin(fs.workingDirectory, fname);
 
 var url = "http://www.momentive.com/products/SelectorLanding.aspx?tid=1792"
 casper.start(url, function() {
     this.echo(this.getTitle());
 });
-
 
 casper.then(function getLinks(){
     brands = this.evaluate(function(){
@@ -26,19 +28,17 @@ casper.then(function(){
     this.each(brands,function(self,brand){
         this.echo(brand);
         link = "http://www.momentive.com" + brand[1]
-
         self.thenOpen(link,function(a){
-            this.echo(this.getCurrentUrl());
-            var products = self.querySelectorAll('a.msds');
-            for (var index = 0; index < products.length; index++) {
-                this.echo(products[i]);
-            }
-            products = Array.prototype.map.call(products,function(link){
-                //return link.getAttribute('href');
-                this.echo('searchResultLink'+link.querySelector('a.searchResultLink'));
-                return  link.querySelector('a.searchResultLink').getAttribute('href');
+            var products = this.evaluate(function(){
+                var products = document.querySelectorAll('a.msds','a.searchResultLink');
+
+                products = Array.prototype.map.call(products,function(link){
+                    return link.getAttribute('href');
+                });
+                return products;
             });
-            this.echo(products);
+            this.echo(products.join('\n'));
+            fs.write(save, '\n' + products.join('\n'), 'a');
         });
     });
 });
@@ -47,5 +47,8 @@ casper.run(function(){
     // echo results in some pretty fashion
     this.echo(brands.length + ' brands found:');
     this.echo(' - ' + brands.join('\n - ')).exit();
+
+    this.echo(products.length + ' products found:');
+
     this.exit();
 });
