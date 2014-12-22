8056bb71b7e506807a7cec0d0d6038b49fd3bada
michalisc <michalis@ravn.co.uk>
2014-12-22 16:54:06 +0000

    Retrives the Products and link to products on all  page

--- fetch-pdfs/casper_fetch_urls.js
+++ fetch-pdfs/casper_fetch_urls.js
@@ -9,12 +9,20 @@ var fname = 'urls.txt';
 var save = fs.pathJoin(fs.workingDirectory, fname);
 
 var url = "http://www.momentive.com/products/SelectorLanding.aspx?tid=1792"
+var segmenturl =  "http://www.momentive.com/products/SelectorLanding.aspx?tid=1211"
+url = segmenturl;
 casper.start(url, function() {
     this.echo(this.getTitle());
 });
 
+
+
+
+
+
+
 casper.then(function getLinks(){
-    // Brand is an array, 0 is brand name, 1 is relative URL: e.g.
+    // Brands is an array, 0 is brand name, 1 is relative URL: e.g.
     // Silbreak*&nbsp;(28),/Products/selectorresults.aspx?taxids=1813&sort=Ascending
     brands = this.evaluate(function(){
         var brands = document.querySelectorAll('a.selectorLandingItemLink');
@@ -28,8 +36,10 @@ casper.then(function getLinks(){
 casper.then(function(){
     this.each(brands,function(self,brand){
         this.echo( brand);
-        link = "http://www.momentive.com" + brand[1]
-        self.thenOpen(link,function(a){
+        // Hack to show all the products on one page (ps=1000)
+        link = "http://www.momentive.com" + brand[1] + "&ps=1000"
+
+        self.thenOpen(link,brand,function(a){
             var products = this.evaluate(function(){
                 var results = document.querySelectorAll('.searchResultBlock');
                 results = Array.prototype.map.call(results,function(link){
@@ -39,14 +49,18 @@ casper.then(function(){
                     return [name,url] ;
                 });
                 return results;
-
-
             });
-            this.echo(products.join('\n'));
             //this.echo(products);
             //fs.write(save, '\n' + products, 'a');
-            fs.write(save, '\n' + products.join('\n'), 'a');
+            this.echo(brand)
+            this.echo(products.join('\n'));
 
+            // Add the Brand to each product
+            for( var i = 0; i < products.length; i++ ) {
+                products[i] =  brand + ',' + products[i];
+            }
+            fs.write(save, 'Brand, BrandURL, Product, ProductURL')
+            fs.write(save, '\n' + products.join('\n'), 'a');
 
         });
     });
