3c0d82647bf300248d8ed78f23473a7e85e5e1f8
michalisc <michalis@ravn.co.uk>
2014-12-23 12:01:10 +0000

    Still some missing products

--- fetch-pdfs/casper_fetch_urls.js
+++ fetch-pdfs/casper_fetch_urls.js
@@ -10,75 +10,84 @@ var casper = require('casper').create({
 var fs = require('fs');
 var url = "http://www.google.com";
 
-links = [[ "Segment","http://www.momentive.com/products/SelectorLanding.aspx?tid=1211"]];
-//links = [ ["Brand","http://www.momentive.com/products/SelectorLanding.aspx?tid=1792"]]
+groupLink =   [ "Segment", "http://www.momentive.com/products/SelectorLanding.aspx?tid=1211"];
+//groupLink = ["Brand", "http://www.momentive.com/products/SelectorLanding.aspx?tid=1792"];
+//groupLink = ["Brand","http://www.momentive.com/products/SelectorLanding.aspx?tid=1792"];
+//groupLink = ["Industry","http://www.momentive.com/products/SelectorLanding.aspx?tid=1172"];
+//groupLink = ["Application","http://www.momentive.com/products/SelectorLanding.aspx?tid=1033"];
+//groupLink = ["Process","http://www.momentive.com/products/SelectorLanding.aspx?tid=1728"];
+
+casper.start(url, function () {
+    this.echo(this.getTitle());
+});
 
 
-for (var i = 0; i < links.length; i++) // for every link...
-{
-    casper.start(url, links, function() {
-        this.echo(this.getTitle());
-    });
+// Create output file and header
+var group = groupLink[0];
+var fname = group + ".csv";
+var save = fs.pathJoin(fs.workingDirectory, fname);
+fs.write(save, group + "," + group + "URL, Product, ProductURL");
 
-    // Create output file and header
-    var group = links[i][0] ;
-    var fname = group + ".csv";
-    var save = fs.pathJoin(fs.workingDirectory, fname);
-    fs.write(save, group + "," + group + "URL, Product, ProductURL");
+var brands = [];
+casper.thenOpen(groupLink[1], brands, function () { // open that link
+    console.log(this.getTitle() + '\n'); // display the title of page
+    brands = brands.concat(this.evaluate(function () {
+        var brands = document.querySelectorAll('a.selectorLandingItemLink');
+        brands = Array.prototype.map.call(brands, function (link) {
+            return [  link.innerHTML, link.getAttribute('href')];
+        });
+        return brands;
+    }));
+});
 
-    var brands = [];
-    casper.thenOpen(links[i][1], brands, function() { // open that link
-        console.log(this.getTitle() + '\n'); // display the title of page
-        brands = brands.concat(this.evaluate(function(){
-            var brands = document.querySelectorAll('a.selectorLandingItemLink');
-            brands = Array.prototype.map.call(brands,function(link){
-                return [ link.innerHTML, link.getAttribute('href')];
-            });
-            return brands;
-        }));
-    });
+casper.then(function () {
+    casper.each(brands, function (self, brand) {
+        casper.then(function () {
+                this.echo(' \t \t ' + brand);
+            }
+        );
+        // Hack to show all the products on one page (ps=1000)
+        link = "http://www.momentive.com" + brand[1] + "&ps=1000";
+        self.thenOpen(link, brand, function (a) {
 
-    casper.then( function(){
-        casper.each(brands,function(self,brand){
-            casper.then(function() {
-                    this.echo(' \t \t ' + brand);
-                }
-            );
-            // Hack to show all the products on one page (ps=1000)
-            link = "http://www.momentive.com" + brand[1] + "&ps=1000"
-            self.thenOpen(link,brand,function(a){
-                var products = this.evaluate(function(){
-                    var results = document.querySelectorAll('.searchResultBlock');
-                    results = Array.prototype.map.call(results,function(link){
-                        url = link.querySelector('a.msds').getAttribute('href');
-                        name = link.querySelector('p.title-info > a.searchResultLink').innerHTML;
-
-                        return [name,url] ;
-                    });
-                    return results;
+            // Find links present on this page
+            var products = this.evaluate(function() {
+                var links = [];
+                Array.prototype.forEach.call(__utils__.findAll('.searchResultBlock'), function(e) {
+                    name = e.querySelector('p.title-info > a.searchResultLink').innerHTML;
+                    url = e.querySelector('a.msds').getAttribute('href');
+                    links.push([ name ? name : "NA" , url? url : "NA"   ]);
                 });
-                //this.echo(products);
-                //fs.write(save, '\n' + products, 'a');
-                this.echo(brand)
-                if (products != null) {
-                    this.echo('\t' + products.join('\n\t'));
-
-                    // Add the Brand to each product
-                    for( var i = 0; i < products.length; i++ ) {
-                        products[i] =  brand + ',' + products[i];
-                    }
-                    fs.write(save, '\n' + products.join('\n'), 'a');
-                }
+                return links;
             });
+
+            this.echo(brand)
+            if (products != null) {
+                this.echo('\t' + products.join('\n\t'));
+
+                // Add the Brand to each product
+                for (var i = 0; i < products.length; i++) {
+                    products[i] = brand + ',' + products[i];
+                }
+                fs.write(save, '\n' + products.join('\n'), 'a');
+            }else {
+                this.echo("********************NO PRODUCTS************************");
+            }
         });
     });
+});
 
-    casper.run(function(){
-        // echo results in some pretty fashion
-        this.echo(brands.length + ' brands found:');
-        this.echo('\n - ' + brands).exit();
+casper.then(function () {
+    // echo results in some pretty fashion
+    this.echo(brands.length + ' brands found:');
+    this.echo('\n - ' + brands).exit();
 
-        this.exit();
-    });
-}
+});
 
+
+casper.run(function () {
+    // echo results in some pretty fashion
+    this.echo(groupLinks.length + ' groups done:');
+
+    this.exit();
+});
